7c24bac78f97cd74281612c2fd5fdfd51d4d5475
Justin Bailey <meticoeus@gmail.com>
2018-07-11 07:08:11 -0400

    refactor: Extract common request logic

--- src/main/java/io/swiftworks/auth0/Auth0HttpClient.java
+++ src/main/java/io/swiftworks/auth0/Auth0HttpClient.java
@@ -4,13 +4,17 @@ import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpClient;
 import io.vertx.core.http.HttpClientOptions;
 import io.vertx.core.http.HttpClientRequest;
+import io.vertx.core.http.HttpClientResponse;
 import io.vertx.core.json.JsonObject;
 
+@SuppressWarnings("WeakerAccess")
 public class Auth0HttpClient {
     private static final String AUTH0_FORWARDED_FOR = "auth0-forwarded-for";
+    private static final String AUTHORIZATION = "Authorization";
 
     protected final String audience;
     protected final String clientId;
@@ -46,52 +50,21 @@ public class Auth0HttpClient {
     }
 
     public void loginUser(LoginUserRequest loginUserRequest, Handler<AsyncResult<JsonObject>> handler) {
-        JsonObject requestBody = new JsonObject();
-        requestBody.put("grant_type", "password");
-        requestBody.put("username", loginUserRequest.getUsername());
-        requestBody.put("password", loginUserRequest.getPassword());
-        requestBody.put("audience", audience); // 'API_IDENTIFIER',
-        requestBody.put("scope", loginUserRequest.getScope());
-        requestBody.put("client_id", clientId);
-        requestBody.put("client_secret", clientSecret);
-
         try {
+            JsonObject requestBody = new JsonObject();
+            requestBody.put("grant_type", "password");
+            requestBody.put("username", loginUserRequest.getUsername());
+            requestBody.put("password", loginUserRequest.getPassword());
+            requestBody.put("audience", audience);
+            requestBody.put("scope", loginUserRequest.getScope());
+            requestBody.put("client_id", clientId);
+            requestBody.put("client_secret", clientSecret);
+
             String url = this.apiUrl + "/oauth/token";
 
-            HttpClientRequest request = this.httpClient.postAbs(url, response -> {
-                try {
-                    response.exceptionHandler(t ->
-                            handler.handle(Future.failedFuture(new RequestException("Failed to log in user", t)))
-                    );
+            HttpClientRequest request = this.httpClient.postAbs(url, buildResponseHandler(handler,"Failed to log in user."));
 
-                    response.bodyHandler(buffer -> {
-                        try {
-                            JsonObject result;
-                            if (buffer.length() > 0) {
-                                result = JsonUtils.toCamelCase(new JsonObject(buffer.toString()));
-                            } else {
-                                result = new JsonObject();
-                            }
-                            result.put("statusCode", response.statusCode());
-
-                            if (response.statusCode() >= 200 && response.statusCode() < 400) {
-                                handler.handle(Future.succeededFuture(result));
-                            } else {
-                                handler.handle(Future.failedFuture(new RequestException(result, response.statusCode())));
-                            }
-
-                        } catch (Throwable t) {
-                            handler.handle(Future.failedFuture(new RequestException("Failed to parse login response. Received: '" + (buffer == null ? "null" : buffer.toString()) + "'", t)));
-                        }
-                    });
-                } catch (Throwable t) {
-                    handler.handle(Future.failedFuture(new RequestException("Failed to log in user. Error binding request handlers.", t)));
-                }
-            });
-
-            request.exceptionHandler(t ->
-                    handler.handle(Future.failedFuture(new RequestException("Failed to connect to server", t)))
-            );
+            request.exceptionHandler(buildExceptionHandler(handler));
 
             request.putHeader(AUTH0_FORWARDED_FOR, loginUserRequest.getIpAddress())
                     .putHeader("content-type", "application/json")
@@ -99,5 +72,46 @@ public class Auth0HttpClient {
         } catch (Throwable t) {
             handler.handle(Future.failedFuture(new RequestException("Failed to send request", t)));
         }
+    }
+
+    private Handler<HttpClientResponse> buildResponseHandler(Handler<AsyncResult<JsonObject>> handler, String errorMessage) {
+        return response -> {
+            try {
+                response.exceptionHandler(t ->
+                        handler.handle(Future.failedFuture(new RequestException(errorMessage, t)))
+                );
+
+                response.bodyHandler(buildBodyHandler(handler, response));
+            } catch (Throwable t) {
+                handler.handle(Future.failedFuture(new RequestException(errorMessage + " Error binding request handlers.", t)));
+            }
+        };
+    }
+
+    private Handler<Buffer> buildBodyHandler(Handler<AsyncResult<JsonObject>> handler, HttpClientResponse response) {
+        return buffer -> {
+            try {
+                JsonObject result;
+                if (buffer.length() > 0) {
+                    result = JsonUtils.toCamelCase(new JsonObject(buffer.toString()));
+                } else {
+                    result = new JsonObject();
+                }
+                result.put("statusCode", response.statusCode());
+
+                if (response.statusCode() >= 200 && response.statusCode() < 400) {
+                    handler.handle(Future.succeededFuture(result));
+                } else {
+                    handler.handle(Future.failedFuture(new RequestException(result, response.statusCode())));
+                }
+
+            } catch (Throwable t) {
+                handler.handle(Future.failedFuture(new RequestException("Failed to parse response. Received: '" + (buffer == null ? "null" : buffer.toString()) + "'", t)));
+            }
+        };
+    }
+
+    private Handler<Throwable> buildExceptionHandler(Handler<AsyncResult<JsonObject>> handler) {
+        return t -> handler.handle(Future.failedFuture(new RequestException("Failed to connect to server", t)));
     }
 }
